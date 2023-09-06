268dc90baa43c693de47ac126915aab6c3f91450
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-09-06 22:25:50 +0300

    Added progress percentage

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ function updateProgress(points) {
     setElementInnerHtml('points-status-goal', goalPoints.toString());
 
     getElementByClass('progress-status').style.width = `${(points > goalPoints ? goalPoints : points) / goalPoints * 100}%`;
+
+    setElementInnerHtml('progress-status-percentage', `${Math.floor(points / goalPoints * 100)}%`);
 }
 
 function getElementByClass(className) {
